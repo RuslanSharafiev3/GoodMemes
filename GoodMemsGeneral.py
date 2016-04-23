7798c11b9c1623a4acc1eb2812e3c842dc8c5635
Ruslan Sharafiev <ruslan.sharafiev3@gmail.com>
2016-04-23 21:22:07 +0400

    second

--- GoodMemsGeneral.py
+++ GoodMemsGeneral.py
@@ -1,12 +1,12 @@
 from tkinter import *
 
+
 root = Tk()
-frame1=Frame(root,width=500,heigh=100,bg='green',bd=5)
-but=Button(frame1,text=u'Первая кнопка')
-var = StringVar()
 info = Text(root, width = 40, height = 2, bg = "#597DA3", fg = "white",
-               font="Verdana 12",
+               font="arial 12",
                wrap=WORD)
+
+fra1 = Frame(root,width=500,height=400,bg="darkred")
 
 but = Button(root,
           text="Next", #надпись на кнопке
@@ -14,19 +14,21 @@ but = Button(root,
           bg="#597DA3",fg="white",
           font = 'arial 10') #цвет фона и надписи
 
-info.insert(1.0, "Hello! Do you see this meme?\n If not select New meme or yes select Old meme.")
+info.insert(1.0, "Hello! Do you seen this meme?\n If not select New meme or yes select Old meme.")
 
 
 var1=IntVar()
 var2=IntVar()
-rbutton1=Radiobutton(root,text='New meme',font = 'arial 10', variable=var,value=1)
-rbutton2=Radiobutton(root,text='Old meme', font = 'arial 10', variable=var,value=2)
+rbutton1=Radiobutton(root,text='New meme',font = 'arial 10', value=1)
+rbutton2=Radiobutton(root,text='Old meme', font = 'arial 10',value=2)
+
 
 but.pack(side = "bottom")
 rbutton2.pack(side = "bottom", fill = "none")
 rbutton1.pack(side = "bottom", fill = "none")
 but.pack(side = "bottom")
+fra1.pack(side = "bottom")
 info.pack(side = "top")
 root.title("GoodMems")
-root.geometry("700x400")
+root.geometry("800x600")
 root.mainloop()
